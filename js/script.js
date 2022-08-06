11914c9709504bdff9ef1f62750b1ac5791b7c58
diadieva <110339735+diadieva@users.noreply.github.com>
2022-08-06 22:04:04 +0200

    start js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,13 @@
-let now = new Date();
-let currentTime = document.querySelector("#date");
-
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
-];
-
-let month = months[now.getMonth()];
-
+function formatDate(timestamp) {
+let date = new Date(timestamp);
+let hours = date.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
 let days = [
   "Sunday",
   "Monday",
@@ -27,41 +17,83 @@ let days = [
   "Friday",
   "Saturday"
 ];
-let day = days[now.getDay()];
+let day = days[date.getDay()];
+return `${day} ${hours}:${minutes}`;
+}
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-let monthDayNumber = now.getDate();
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "67463d90019f3c15672ebddc2b82fac3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+let currentTime = document.querySelector("#date");
+
+
+
+
+
+let monthDayNumber = date.getDate();
 if (monthDayNumber < 10) {
   monthDayNumber = `0${monthDayNumber}`;
 }
 
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
-}
 
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
 let time = document.querySelector("h3");
 time.innerHTML = `${day} ${monthDayNumber} ${month}, ${hours}:${minutes}`;
 
-function showWeather(response) {
-  let city = response.data.name;
-  let cityElement = document.querySelector("#location");
-  cityElement.innerHTML = city;
-  let temperature = Math.round(response.data.main.temp);
-  let tempElement = document.querySelector("#temperatureNumber");
-  tempElement.innerHTML = temperature;
-  let humidity = response.data.main.humidity;
-  let humidityElement = document.querySelector("#humidity");
-  humidityElement.innerHTML = humidity;
-  let wind = Math.round(response.data.wind.speed);
-  let windElement = document.querySelector("#wind");
-  windElement.innerHTML = wind;
-  let description = response.data.weather[0].main;
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
-  descriptionElement.innerHTML = description;
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
 }
 function handleSubmit(event) {
   event.preventDefault();
